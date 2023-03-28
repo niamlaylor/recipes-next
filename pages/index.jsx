2c2c1f0d8cd6113bc91b80479f787b9271fc4894
Liam Naylor <liam@liamnaylor.com>
2023-03-28 14:18:14 -0700

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link';
+import Navigation from '../components/Navigation';
+
+export default function Home() {
+  return (
+    <Navigation/>
+  )
+}
