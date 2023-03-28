e90fa131a412a30f6897e87974424a1c876d2138
Liam Naylor <liam@liamnaylor.com>
2023-03-28 14:23:14 -0700

    Clean up to remove test component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Link from 'next/link';
-import Navigation from '../components/Navigation';
 
 export default function Home() {
-  return (
-    <Navigation/>
-  )
+  return
 }
