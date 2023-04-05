2f7eb44af9dcc5ce77cd4be9d5c0521279183633
Chelsea Dwarika <chelseakristina@live.com>
2023-04-05 00:30:50 +0000

    full recipe card css styling

--- components/recipe-card/RecipeCard.jsx
+++ components/recipe-card/RecipeCard.jsx
@@ -4,18 +4,43 @@ import StepList from './steps/StepList'
 
 export default function RecipeCard({ recipe }) {
   return (
-    <article className={styles.recipeFullCard}>
-      <h3>{recipe.name}</h3>
-      <label>from {recipe.website}</label>
-      <div>
-        <h5>Ingredients</h5>
-        <IngredientList ingredients={recipe.ingredients}/>
-      </div>
-      <div>
-        <h5>Steps</h5>
-        <StepList steps={recipe.steps}/>
-      </div>
-    </article>
+    <div className={styles.recipeFullCard}>
+    <div className={styles.recipe}>
 
+      <div className={styles.circle_recipe}>
+      <p>Cooking Time</p>
+        <h2>{recipe.duration} mins</h2>
+      </div>
+      <div className={styles.circle_recipe}>
+        <p>Serves</p>
+        <h2>12</h2>
+      </div>
+      
+    <div className={styles.content}>
+
+
+
+    <div className={styles.row}>
+    <div className={styles.description}>
+      <h2>{recipe.name}</h2>
+      <p>{recipe.website}</p>
+    </div>
+
+
+      <div className={styles.column} 
+          id="ingredients">
+      <h2>Ingredients</h2>
+      <IngredientList ingredients={recipe.ingredients}/>
+       </div>
+       
+      <div className={styles.column}
+          id="method">
+      <h2>Method</h2>
+    <StepList steps={recipe.steps}/>
+      </div>
+</div>
+</div>
+      </div>
+    </div>
   );
 };
