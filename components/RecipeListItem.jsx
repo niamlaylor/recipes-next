3c084ed3b5cee5dc5d64e49e90a04f33221da5dc
Chelsea Dwarika <chelseakristina@live.com>
2023-04-05 12:25:45 +0000

    add grid layout for List cards

--- components/RecipeListItem.jsx
+++ components/RecipeListItem.jsx
@@ -42,7 +42,7 @@ export default function RecipeListItem({id, name, website, duration, labels, ima
 
   return (
     <ThemeProvider theme={theme}>
-      <Card style={{backgroundColor: theme.palette.secondary.main}} sx={{ maxWidth: 400 }} onClick={() => handleRecipeClick(id)}>
+      <Card style={{backgroundColor: theme.palette.secondary.main}} sx={{ maxWidth: 350 }} onClick={() => handleRecipeClick(id)}>
       <CardMedia
         sx={{ height: 150 }}
         image="https://pixabay.com/get/g227b4122014848e19fd9e9bbb1eb3204cd5e112cba2ca935d2c145abbcfc3bec55efb02ca2e9489e93d2e201f62e89707f57cfb3ae294cf8df858030be013ba8689ae8774a5c3cc9eb6ba1bf8693f9f8_640.jpg"
@@ -58,14 +58,15 @@ export default function RecipeListItem({id, name, website, duration, labels, ima
         <Typography gutterBottom variant="body2" component="div">
           {duration} mins
         </Typography>
-        <CardActions disableSpacing >
+
           <IconButton aria-label="edit" sx={{color: theme.palette.primary.main}}>
             <EditIcon />
           </IconButton>
+
           <IconButton aria-label="delete" sx={{color: theme.palette.primary.main}}>
             <DeleteIcon />
           </IconButton>
-        </CardActions>
+
       </CardContent>
       </Card>
     </ThemeProvider>
