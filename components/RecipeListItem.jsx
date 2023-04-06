bae25fa9fdbc9195f4b04eaa2ec80f723ea41e19
Paige Nelmes <designer@paigenelmes.com>
2023-04-06 12:50:27 -0700

    Styled recipe list items

--- components/RecipeListItem.jsx
+++ components/RecipeListItem.jsx
@@ -1,6 +1,36 @@
-import styles from '../styles/RecipeListItem.module.css';
+//import styles from '../styles/RecipeListItem.module.css';
 import LabelList from './LabelList';
 import { useRouter } from 'next/router';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { IconButton } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+
+const theme = createTheme({
+  palette: {
+    //Main colour, dark brown
+    primary: {
+      main: '#542307'
+    },
+    //Secondary colour, light brown with reduced opacity
+    secondary: {
+      main: 'rgba(220, 204, 192, 0.5)'
+    }
+  },
+  //Nunito Sans font
+  typography: {
+    fontFamily: 'Nunito Sans',
+    fontWeightRegular: 400,
+    fontWeightBold: 700
+  }
+});
 
 export default function RecipeListItem({id, name, website, duration, labels, image}) {
 
@@ -11,14 +41,45 @@ export default function RecipeListItem({id, name, website, duration, labels, ima
   }
 
   return (
-    <li className={styles.recipeListCard} onClick={() => handleRecipeClick(id)}>
+    <ThemeProvider theme={theme}>
+      <Card style={{backgroundColor: theme.palette.secondary.main}} sx={{ maxWidth: 400 }} onClick={() => handleRecipeClick(id)}>
+      <CardMedia
+        sx={{ height: 150 }}
+        image="https://pixabay.com/get/g227b4122014848e19fd9e9bbb1eb3204cd5e112cba2ca935d2c145abbcfc3bec55efb02ca2e9489e93d2e201f62e89707f57cfb3ae294cf8df858030be013ba8689ae8774a5c3cc9eb6ba1bf8693f9f8_640.jpg"
+        title="placeholder"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2">
+          From {website}
+        </Typography>
+        <Typography gutterBottom variant="body2" component="div">
+          {duration} mins
+        </Typography>
+        <CardActions disableSpacing >
+          <IconButton aria-label="edit" sx={{color: theme.palette.primary.main}}>
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="delete" sx={{color: theme.palette.primary.main}}>
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
+      </CardContent>
+      </Card>
+    </ThemeProvider>
+  );
+}
+
+{/* ORIGINAL CODE */}
+
+{/* <li className={styles.recipeListCard} onClick={() => handleRecipeClick(id)}>
       <div className={styles.recipeTitleSource}>
         <h3>{name}</h3>
         <label>from {website}</label>
         <label>{duration}m</label>
-        {/* <LabelList labels={labels}/> */}
-      </div>
-      <img className={styles.recipeImage} src={image}></img>
-    </li>
-  );
-}
+        {<LabelList labels={labels}/>} THIS LINE WAS COMMENTED OUT
+        </div>
+        <img className={styles.recipeImage} src={image}></img>
+      </li> */}
