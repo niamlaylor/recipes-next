e6ffb719bf7d3d0fbf34e0a2397128e4ab4834e7
Cassandra Keddis <cassandra@thinkific.com>
2023-04-06 09:13:41 -0700

    create a record in the db with a url

--- components/forms/AddRecipeForm.jsx
+++ components/forms/AddRecipeForm.jsx
@@ -1,13 +1,43 @@
 import styles from '../../styles/forms/AddRecipeForm.module.css'
 import { useState } from "react";
+import axios from 'axios';
 
 export default function AddRecipeForm(props) {
 
   const [website, setWebsite] = useState(props.website || "");
 
+  const getRecipe = async (event) => {
+    event.preventDefault();
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify({
+        url: event.target.url.value
+      })
+    };
+    const recipeData = await fetch("/api/get-recipe/", fetchOptions).then(
+      (res) => {
+        res.json().then((recipeJson) => {
+          console.log(typeof recipeJson);
+          axios.post('/api/recipes', recipeJson).then((response) => {
+            console.log("Axios request successful");
+            console.log(response);
+          }).catch((error) => {
+            console.log("Axios request error");
+            console.log(error);
+          });
+        });
+      }
+    );
+    return recipeData;
+  };
+
   return (
     <section className={styles.formSection}>
-      <form className={styles.addRecipeForm} onSubmit={event => event.preventDefault()} autoComplete="off">
+      <form className={styles.addRecipeForm} onSubmit={getRecipe} autoComplete="off">
         <input
           className ={styles.addRecipeInput}
           name="url"
