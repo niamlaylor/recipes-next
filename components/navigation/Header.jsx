31c06df42e6ab6d251437734d6002bedc26a8be4
Chelsea Dwarika <chelseakristina@live.com>
2023-04-07 11:02:16 +0000

    Header CSS -> MUI

--- components/navigation/Header.jsx
+++ components/navigation/Header.jsx
@@ -1,3 +1,99 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import { useSession, signOut } from 'next-auth/react';
+import { createTheme } from '@mui/material/styles';
+import NavLink from "./NavLink"
+
+const theme = createTheme({
+  palette: {
+    //Main colour, dark brown
+    primary: {
+      main: '#542307'
+    },
+    //Secondary colour, lighter brown
+    secondary: {
+      main: '#DCCCC0'
+    }
+  },
+  //Nunito Sans font
+  typography: {
+    fontFamily: 'Nunito Sans',
+    fontWeightRegular: 400,
+    fontWeightBold: 700
+  }
+});
+
+export default function Header() {
+
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{backgroundColor: theme.palette.secondary.main}}>
+       
+        <Toolbar>
+          
+          <Typography variant="body1" component="div" sx={{ flexGrow: 1, color: theme.palette.primary.main }}>
+          <NavLink href={"/account"}>{session.user.name}</NavLink>'s List
+          </Typography>
+          
+          <Typography align='center'>
+            <Link href="/">
+              <Box
+        component="img"
+        sx={{ height: 54 }}
+        alt="Logo"
+        src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"/>
+            </Link>
+          </Typography>
+             
+          <Button onClick={() => signOut()}>Sign out</Button>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+
+} else {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static" style={{backgroundColor: theme.palette.secondary.main}}>
+     
+      <Toolbar>
+
+      <Typography variant="body1" component="div" sx={{ flexGrow: 1, color: theme.palette.primary.main }}>
+          </Typography>
+
+        
+        <Typography align='center'>
+          <Link href="/">
+            <Box
+      component="img"
+      sx={{ height: 54 }}
+      alt="Logo"
+      src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"/>
+          </Link>
+        </Typography>
+
+          <Button href={"/login"} style={{backgroundColor: theme.palette.secondary.main}}>Login</Button>
+          <Button href={"/signup"} style={{backgroundColor: theme.palette.secondary.main}}>Sign Up</Button>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+}
+
+
+/* OLD HEADER
+
 import { headerNav, headerNavList, headerNavItem } from "../../styles/Header.module.css"
 import NavLink from "./NavLink"
 import Link from "next/link";
@@ -15,7 +111,7 @@ export default function Header() {
         <nav className={headerNav} role="navigation" aria-label="Site">
           <ul className={headerNavList}>
             <li className={headerNavItem}>
-            {/* H6 Typography for main links */}
+ H6 Typography for main links 
               <Typography variant ="h6" href={"/"} >My List</Typography>
             </li>
             <li id="sifterLogo">
@@ -23,7 +119,7 @@ export default function Header() {
                 <img src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"></img>
               </Link>
             </li>
-            {/* Button Styles for Login & Sign Up Links */}
+ Button Styles for Login & Sign Up Links
             <li>
               <NavLink href={"/account"}>Welcome {session.user.name}</NavLink>
               <Button variant="contained" size="large" onClick={() => signOut()}>Sign out</Button>
@@ -38,7 +134,7 @@ export default function Header() {
         <nav className={headerNav} role="navigation" aria-label="Site">
           <ul className={headerNavList}>
             <li className={headerNavItem}>
-            {/* H6 Typography for main links */}
+H6 Typography for main links
               <Typography variant ="h6" href={"/"} >My List</Typography>
             </li>
             <li id="sifterLogo">
@@ -60,3 +156,4 @@ export default function Header() {
     );
   }
 };
+*/
