f927c2bd692745c3b5eeea37d9a0530ac2a9381d
Liam Naylor <liam@liamnaylor.com>
2023-04-09 13:18:38 -0700

    Add google auth sign in functionality + updated schema

--- pages/api/users.jsx
+++ pages/api/users.jsx
@@ -1,17 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const { name, email, password } =
-        req.body;
-
-      const user = await prisma.recipe.create({
+      const { name, email, password, confirmPassword } = req.body;
+      console.log(`Received ${req.method} request to create a user: `, name, email, password, confirmPassword);
+      const user = await prisma.user.create({
         data: {
           name,
           email,
           password
-        },
+        }
       });
-      res.status(200).json(user);
+
+      const updatedUser = JSON.stringify(
+        user,
+        (key, value) => (typeof value === 'bigint' ? value.toString() : value)
+      )
+      res.status(200).json(updatedUser);
     } catch (error) {
       res.status(500).json({ message: 'Something went wrong' });
     };
