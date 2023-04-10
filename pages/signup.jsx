e4e27e32d41447711ee8700af545b7cac9661ec8
Liam Naylor <liam@liamnaylor.com>
2023-04-10 09:16:55 -0700

    Add basic condit. rendering for login/signup forms

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -14,110 +14,122 @@ import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import Header from '../components/navigation/Header';
 import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function SignUp( { theme }) {
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const { fullName, email, password, confirmPassword } = event.target;
-    const user = {
-      name: fullName.value,
-      email: email.value,
-      password: password.value,
-      confirmPassword: confirmPassword.value,
-    };
-    console.log(user)
-    try {
-      await axios.post('/api/users', user)
-      console.log('Sent POST request for this user: ', user)
-    } catch (error) {
-      console.log("Axios request error: ", error);
-    }
-  }
+  const { data: session } = useSession();
+  const router = useRouter();
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Header />
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    autoComplete="full-name"
-                    name="fullName"
-                    required
-                    fullWidth
-                    id="fullName"
-                    label="Full Name"
-                    autoFocus
-                  />
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const { fullName, email, password, confirmPassword } = event.target;
+  //   const user = {
+  //     name: fullName.value,
+  //     email: email.value,
+  //     password: password.value,
+  //     confirmPassword: confirmPassword.value,
+  //   };
+  //   console.log(user)
+  //   try {
+  //     await axios.post('/api/users', user)
+  //     console.log('Sent POST request for this user: ', user)
+  //   } catch (error) {
+  //     console.log("Axios request error: ", error);
+  //   }
+  // }
+
+
+  if (!session) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Header />
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign up
+              </Typography>
+              <Box component="form" noValidate sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="full-name"
+                      name="fullName"
+                      required
+                      fullWidth
+                      id="fullName"
+                      label="Full Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="confirmPassword"
+                      label="Confirm password"
+                      type="password"
+                      id="confirmPassword"
+                      autoComplete="new-password"
+                    />
+                  </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link href="/login" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="confirmPassword"
-                    label="Confirm password"
-                    type="password"
-                    id="confirmPassword"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="/login" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-    </ThemeProvider>
-  );
+          </Container>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <div>You are already signed in!</div>
+    )
+  }
 }
