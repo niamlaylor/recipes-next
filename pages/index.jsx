f915a6f29aa09d6d0073b8ab6465d251a816148e
Liam Naylor <liam@liamnaylor.com>
2023-04-12 11:06:23 -0700

    Show only users created recipes using context

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,13 +3,27 @@ import axios from 'axios';
 import RecipeList from '../components/RecipeList';
 import Header from '../components/navigation/Header';
 import AddRecipeForm from '../components/forms/AddRecipeForm';
+import { getSession } from 'next-auth/react';
 
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function getServerSideProps() {
-  let recipes = await prisma.recipe.findMany();
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+  let recipes = await prisma.recipe.findMany({
+    where: {
+      userId: session.user.id
+    }
+  });
   recipes = recipes.map(recipe => {
     return {
       ...recipe,
