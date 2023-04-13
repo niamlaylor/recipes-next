fa2f5a5d446ca28411f68204179a4a15436cb446
Chelsea Dwarika <chelseakristina@live.com>
2023-04-13 13:58:10 -0400

    initial search bar on header

--- components/navigation/Header.jsx
+++ components/navigation/Header.jsx
@@ -6,8 +6,54 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 import { useSession, signOut } from 'next-auth/react';
-import NavLink from "./NavLink"
+import NavLink from "./NavLink";
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
 
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "#DCCCC0",
+  '&:hover': {
+    backgroundColor: "white",
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "#542307",
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function Header() {
 
@@ -31,7 +77,21 @@ export default function Header() {
           <NavLink href={"/account"}>{session.user.name}</NavLink>'s List
           </Typography>
 
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+
+
+
+
           <Button sx={{ ml: 2 }} variant="outlined" onClick={() => signOut()}>Sign out</Button>
+
 
         </Toolbar>
       </AppBar>
