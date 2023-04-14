868abaf17ef227312fcbb8128234f43b5bc56f16
Cassandra Keddis <cassandra@thinkific.com>
2023-04-13 19:33:48 -0700

    pull and show tags from db instead of local

--- components/recipe-card/Tags.jsx
+++ components/recipe-card/Tags.jsx
@@ -1,37 +1,55 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
+export default function TagsInput(props) {
+  const [tags, setTags] = useState([]);
 
-export default function TagsInput(){
-    const [tags, setTags] = useState([])
+  useEffect(() => {
+    const getLabels = async () => {
+      try {
+        const response = await fetch(`/api/recipes?id=${props.id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const recipeData = await response.json();
+          setTags(recipeData.labels);
+        } else {
+          throw new Error('Failed to fetch recipe labels');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getLabels();
+  }, [props.id]);
 
-    function handleKeyDown(e){
-        if(e.key !== 'Enter') return
-        const value = e.target.value
-        if(!value.trim()) return
-        setTags([...tags, value])
-        e.target.value = ''
-    }
 
-    function removeTag(index){
-        setTags(tags.filter((el, i) => i !== index))
-    }
+  function handleKeyDown(e) {
+    if (e.key !== 'Enter') return;
+    const value = e.target.value;
+    if (!value.trim()) return;
+    setTags([...tags, value]);
+    e.target.value = '';
+  }
 
-    return (
-      <Box>
-        <Stack direction="row" spacing={1}>
-          { tags.map((tag, index) => (
-              <Chip key={index}
-                    label={tag+" ×"} 
-                    onClick={() => removeTag(index)}/>
-          )) }
-          <TextField variant="standard" size="small" sx={{ pl: 2 }} onKeyDown={handleKeyDown} placeholder="Add a tag" />
+  function removeTag(index) {
+    setTags(tags.filter((el, i) => i !== index));
+  }
+
+  return (
+    <Box>
+      <Stack direction="row" spacing={1}>
+        {tags.map((tag, index) => (
+          <Chip key={index} label={tag + ' ×'} onClick={() => removeTag(index)} />
+        ))}
+        <TextField variant="standard" size="small" sx={{ pl: 2 }} onKeyDown={handleKeyDown} placeholder="Add a tag" />
       </Stack>
-      </Box>
-  )
+    </Box>
+  );
 }
-
-
