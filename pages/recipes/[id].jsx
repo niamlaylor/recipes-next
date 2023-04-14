076e52597797fb8801816e17a9944ab4e4fc92d2
Liam Naylor <liam@liamnaylor.com>
2023-04-14 13:39:39 -0700

    Add handling for /recipes/[id] urls not belonging to user

--- pages/recipes/[id].jsx
+++ pages/recipes/[id].jsx
@@ -1,17 +1,39 @@
 import RecipeCard from "../../components/recipe-card/RecipeCard";
 import Header from '../../components/navigation/Header';
+import { getSession } from 'next-auth/react';
+import { checkIfBelongsToUser } from "../../utils/helpers";
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function getServerSideProps(context) {
+
+  // Context passes the session object from the FE over to BE so we can check if it exists
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  };
+
   const { id } = context.params;
 
   const recipe = await prisma.recipe.findUnique({
     where: { id: Number(id) },
   });
 
+  // If no recipe found, trigger 404 page
   if (!recipe) {
+    return { notFound: true };
+  }
+
+  // Make sure /recipe/[id] value belongs to the user
+  const belongsToUser = await checkIfBelongsToUser(session, prisma, id);
+  if (!belongsToUser) {
     return { notFound: true };
   }
 
@@ -28,7 +50,6 @@ export async function getServerSideProps(context) {
     },
   };
 }
-
 
 export default function Recipe({ recipe = [] }) {
   if (!recipe) {
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,18 @@ export function formattedDomain (website) {
   const prettyUrl = url.split('www.')[1];
   return prettyUrl;
 };
+
+// Creates an array of the user's recipe IDs then checks if the URL param id exists in the array
+export const checkIfBelongsToUser = async (sessionValue, prisma, urlId) => {
+  const userRecipes = await prisma.recipe.findMany({
+    where: { 
+      userId: sessionValue.user.id 
+    },
+  });
+
+  const paths = userRecipes.map((recipe) => {
+    return { id: recipe.id.toString() };
+  });
+
+  return paths.find(recipe => recipe.id == urlId );
+};
