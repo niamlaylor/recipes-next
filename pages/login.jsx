cbdbb33bff322befa5df0026e2d8e608e497eb2c
Cassandra Keddis <cassandra@thinkific.com>
2023-04-13 17:26:15 -0700

    add useEffect for redirect, remove login fields

--- pages/login.jsx
+++ pages/login.jsx
@@ -8,7 +8,6 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
@@ -17,11 +16,15 @@ import AuthButton from '../components/forms/AuthButton';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
-
 export default function LogIn({ theme }) {
-
   const { data: session } = useSession();
   const router = useRouter();
+
+  React.useEffect(() => {
+    if (session) {
+      router.push('/');
+    }
+  }, [session]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -30,10 +33,6 @@ export default function LogIn({ theme }) {
       email: data.get('email'),
       password: data.get('password'),
     });
-  };
-
-  const handleRedirect = async (urlPath) => {
-    await router.push(`${urlPath}`);
   };
 
   if (!session) {
@@ -56,47 +55,10 @@ export default function LogIn({ theme }) {
               Log in
             </Typography>
             <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-
               <AuthButton authProvider={'google'} />
               <AuthButton authProvider={'github'} />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
               <Grid container>
                 <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Sign up"}
-                  </Link>
                 </Grid>
               </Grid>
             </Box>
@@ -104,31 +66,20 @@ export default function LogIn({ theme }) {
         </Container>
       </ThemeProvider>
     );
-  } else {
-    return (
-      <ThemeProvider theme={theme}>
-
-        <Header onLoggedInPage={true} userLoggedIn={false}/>
-        <Box sx={{
-            marginTop: 6,
-            marginBottom: 6,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Typography variant="h6">You are already logged in!</Typography>
-          <Button
-            type="button"
-            variant="contained"
-            onClick={() => handleRedirect('/')}
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Home
-          </Button>
-        </Box>
-
-      </ThemeProvider>
-    );
   }
-  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header onLoggedInPage={true} userLoggedIn={false}/>
+      <Box sx={{
+        marginTop: 6,
+        marginBottom: 6,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <Typography variant="h6">You are already logged in!</Typography>
+      </Box>
+    </ThemeProvider>
+  );
 }
