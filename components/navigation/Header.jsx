5cc81eccba167bfe05169e5fe9d4c28c4a61c821
Cassandra Keddis <cassandra@thinkific.com>
2023-04-18 19:09:00 -0700

    code cleanup/refactor

--- components/navigation/Header.jsx
+++ components/navigation/Header.jsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSession, signOut } from 'next-auth/react';
 import AppBar from '@mui/material/AppBar';
+import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
-import { useSession, signOut } from 'next-auth/react';
-import { useState } from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import { useRouter } from 'next/router';
 
 export default function Header() {
 
@@ -29,63 +28,57 @@ export default function Header() {
   if (session) {
     return (
       <Box>
-      <AppBar position="static" style={{ backgroundColor: '#DCCCC0' }}>
-        <Toolbar>
-          <Link href="/" sx={{flexGrow: 1}}> 
-            <Box
-              component="img"
-              sx={{ height: 54 }}
-              alt="Logo"
-              src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"/>
-          </Link>
-
-          <Typography variant="h6" component="div" sx={{color: '#542307' }} >
-            {session.user.name}'s List
-          </Typography>
-
-          <Autocomplete
-            onInputChange={handleInputChange}
-            sx={{ pl: 3, minWidth: "200px" }}
-            freeSolo
-            options={searchQuery && searchResults.length < 1 ? [{ title: "No results" }] : searchResults}
-            getOptionLabel={(option) => option.title}
-            getOptionSelected={(option, value) => option.id === value.id}
-            onChange={(event, value) => {
-              if (value && value.id) {
-                router.push(`/recipes/${value.id}`);
-              }
-            }}
-            renderInput={(params) => <TextField {...params} label="Search..." />}
-          />
-          <Button sx={{ ml: 2 }} variant="outlined" onClick={() => signOut()}>Sign out</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-
-} else {
-  return (
-    <Box>
-    <AppBar position="static" style={{backgroundColor: '#DCCCC0'}}>
-     
-     <Toolbar>
-
-    <Link href="/" sx = {{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ backgroundColor: '#DCCCC0' }} sx={{ paddingTop: '5px' }}>
+          <Toolbar>
+            <Link href="/" sx={{ flexGrow: 1 }}> 
               <Box
-        component="img"
-        sx={{ height: 54 }}
-        alt="Logo"
-        src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"/>
+                component="img"
+                sx={{ height: 54 }}
+                alt="Logo"
+                src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"
+              />
             </Link>
-
-            <Typography variant="body1" component="div" sx={{color: '#542307'}}>
-          </Typography>
-
-          <Button variant="outlined" href={"/login"} style={{backgroundColor: '#DCCCC0'}}>Signup / Login</Button>
-
-        </Toolbar>
-      </AppBar>
+  
+            <Typography variant="h6" component="div" sx={{ color: '#542307' }}>
+              {session.user.name}'s List
+            </Typography>
+  
+            <Autocomplete
+              onInputChange={handleInputChange}
+              sx={{ pl: 3, minWidth: "200px" }}
+              freeSolo
+              options={searchQuery && searchResults.length < 1 ? [{ title: "No results" }] : searchResults}
+              getOptionLabel={(option) => option.title}
+              getOptionSelected={(option, value) => option.id === value.id}
+              onChange={(event, value) => {
+                if (value?.id) {
+                  router.push(`/recipes/${value.id}`);
+                }
+              }}
+              renderInput={(params) => <TextField {...params} label="Search..." />}
+            />
+  
+            <Button sx={{ ml: 2 }} variant="outlined" onClick={signOut}>
+              Sign out
+            </Button>
+          </Toolbar>
+        </AppBar>
       </Box>
-  );
-}
+    );
+  } else {
+    return (
+      <Box>
+        <AppBar position="static" style={{ backgroundColor: '#DCCCC0' }}>
+          <Toolbar>
+            <Link href="/" sx={{ flexGrow: 1 }}>
+              <Box component="img" sx={{ height: 54 }} alt="Logo" src="https://raw.githubusercontent.com/niamlaylor/recipes-next/main/public/sifterLogo.png"/>
+            </Link>
+            <Button variant="outlined" href={"/login"} style={{ backgroundColor: '#DCCCC0', marginLeft: 'auto' }}>
+              Sign up / Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    )
+  };
 }
